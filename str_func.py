5cd6435f3a90f82c8ebf0f37a2d1080631cae83c
Dmitry Khalmatov <hds10@mail.ru>
2024-01-21 15:00:19 +0800

    new_function

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def capital_letters(value):
+    '''Принимает строку value и возращает строку в верхнем регистре'''
+    return value.upper()
+
+
+def first_title(text):
+    '''Принимает строку text и возращает каждое отдельное слово с заглавной буквы'''
+    return text.title()
